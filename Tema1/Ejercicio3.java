0632518d94081063c63b32f3caa640346934619b
Tristan-Indigo <tristanindigo7@gmail.com>
2026-09-22 16:34:44 +0200

    Add files via upload

---
+++ Tema1/Ejercicio3.java
@@ -0,0 +1,7 @@
+public class Ejercicio3 {
+	public static void main(String[] args) {
+		System.out.println("Mi nombre");
+		System.out.println("Mi dirección");
+		System.out.println("Mi numero de telefono");
+	}
+}
