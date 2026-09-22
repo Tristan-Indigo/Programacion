29b292c95478f3361ddd6ca7cdef6cfcd07b545c
Tristan-Indigo <tristanindigo7@gmail.com>
2026-09-22 19:08:52 +0200

    Add files via upload

---
+++ Tema1/Ejercicio5.java
@@ -0,0 +1,14 @@
+public class Ejercicio5 {
+	public static void main(String[] args) {
+		System.out.println("\t \t Lunes \t \t Martes \t Miercoles \t Jueves \t Viernes");
+    	System.out.println("----------------------------------------------------------------------------------------");
+		System.out.println("08:00-09:00 \t LM \t \t BD \t \t SI \t \t PR \t \t ED");
+        System.out.println("09:00-10:00 \t LM \t \t BD \t \t SI \t \t PR \t \t BD");
+        System.out.println("10:00-11:00 \t SI \t \t SOS \t \t ITEC \t \t LM \t \t BD");
+        System.out.println("11:00-11:30 \t Recreo \t Recreo \t Recreo \t Recreo \t Recreo");
+        System.out.println("11:30-12:30 \t SI \t \t ED \t \t ITEC \t \t SI \t \t ITEC");
+        System.out.println("12:30-13:30 \t PR \t \t PR \t \t DIG \t \t BD \t \t PR");
+        System.out.println("13:30-14:30 \t PR \t \t PR \t \t ED \t \t BD \t \t PR");
+        
+	}
+}
