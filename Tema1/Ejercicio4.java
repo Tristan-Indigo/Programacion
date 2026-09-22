241cd91dc448a6d2cc5cbf60fd447f3255c35153
Tristan-Indigo <tristanindigo7@gmail.com>
2026-09-22 16:49:23 +0200

    Add files via upload

---
+++ Tema1/Ejercicio4.java
@@ -0,0 +1,16 @@
+public class Ejercicio4 {
+	public static void main(String[] args) {
+		System.out.println("Palabra en ingles \t Significado");
+		System.out.println("------------------------------------");
+		System.out.println("Hello \t \t \t Hola");
+		System.out.println("World \t \t \t Mundo");
+        System.out.println("Egg \t \t \t Huevo");
+        System.out.println("Soup \t \t \t Sopa");
+        System.out.println("Keyboard \t \t Teclado");
+        System.out.println("Food \t \t \t Comida");
+        System.out.println("Forgotten \t \t Olvidado");
+        System.out.println("Blue \t \t \t Azul");
+        System.out.println("Null \t \t \t Nulo");
+        System.out.println("Yes \t \t \t Si");
+	}
+}
